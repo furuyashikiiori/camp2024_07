348fb31bfcdd42296e7642a62d10e315bb3bde5d
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-07-13 23:32:22 +0900

    お試しでカウンター作ってみた

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import MyComponent from "./MyComponent";
+import MyComponent from "./components/MyComponent";
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyComponent />
+        <Counter />
       </header>
     </div>
   );
