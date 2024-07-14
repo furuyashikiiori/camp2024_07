a18331cbb7e66eddb927b15cf10125b694f47513
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-07-14 10:57:39 +0900

    headerの位置変更

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,16 +9,16 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <header className="App-header">
+          <MyComponent />
+        </header>
+
         {/* <Link>タグで画面遷移のリンクを作成 */}
         <Link to="/home">Home</Link>
         <br />
         <Link to="/counter">Counter</Link>
         <br />
         <Link to="/">MyComponent</Link>
-
-        <header className="App-header">
-          <MyComponent />
-        </header>
 
         {/* <Route>タグでパスに応じたコンポーネントを表示 */}
         <Routes>
