6b687e8c95dc59001f28a447777f23d162d3d2c9
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2024-07-15 13:51:55 +0900

    Revert "Merge pull request #11 from furuyashikiiori/features-todo-api-mitsuki"
    This reverts commit 85d1fe53f4201b7c069910a0d55cad60cba26a52, reversing
    changes made to 44bd9ffb214cc39c8a2a903c806edf067df6475b.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,12 +5,26 @@ import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MyComponent />
-        <Counter />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <MyComponent />
+        </header>
+
+        {/* <Link>タグで画面遷移のリンクを作成 */}
+        <Link to="/home">Home</Link>
+        <br />
+        <Link to="/counter">Counter</Link>
+        <br />
+        <Link to="/">MyComponent</Link>
+
+        {/* <Route>タグでパスに応じたコンポーネントを表示 */}
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
