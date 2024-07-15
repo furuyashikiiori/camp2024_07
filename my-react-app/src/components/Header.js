30461cb6aaeed3169aec6baa80981ed09482fe59
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2024-07-16 01:08:46 +0900

    nav追加

--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Header.css';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
       <h1>Todo List</h1>
+      <nav>
+        <ul>
+            {/* <Link>タグで画面遷移のリンクを作成 */}
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/counter">Counter</Link></li>
+            <li><Link to="/">MyComponent</Link></li>
+            <li><Link to="/todo">Todo</Link></li>
+        </ul>
+    </nav>
     </header>
   );
 };
