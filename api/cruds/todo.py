027b02f33b55dc825fb1279e4edd7be58aab1bdb
mitsuki0215 <mitsuki0215>
2024-07-18 00:58:38 +0900

    todoの作成・取得・更新・削除・名前で検索機能のAPIを完成させた

--- api/cruds/todo.py
+++ api/cruds/todo.py
@@ -1,55 +1,60 @@
-from typing import List, Optional
-from schemas import ToDoCreate, ToDoUpdate, ToDoResponse, ToDoStatus, ToDoCategory
+from sqlalchemy.orm import Session
+from typing import Optional, List
+from schemas import ToDoCreate, ToDoUpdate, ToDoResponse, ToDoCategory
+from models import Todo
 
-class Todo:
-    def __init__(self, id: int, name: str, category: ToDoCategory, status: ToDoStatus, due_date: Optional[str] = None):
-        self.id = id
-        self.name = name
-        self.category = category
-        self.status = status
-        self.due_date = due_date
+def find_all(db: Session) -> List[Todo]:
+    return db.query(Todo).all()
 
-todos = [
-    Todo(id=1, name="課題をやる", category=ToDoCategory.SCHOOL, status=ToDoStatus.TASK_PENDING, due_date="2024/07/14/12:00"),
-    Todo(id=2, name="掃除をする", category=ToDoCategory.HOUSEHOLD, status=ToDoStatus.TASK_PENDING, due_date="2024/07/14/15:00"),
-]
+def find_by_id(db:Session, id: int) -> Optional[Todo]:
+    return db.query(Todo).filter(Todo.id == id).first()
 
-def find_all() -> List[Todo]:
-    return todos
+def find_by_name(db: Session, name: str) -> List[Todo]:
+    return db.query(Todo).filter(Todo.name.like(f"%{name}%")).all()
 
-def find_by_id(item_id: int) -> Optional[Todo]:
-    for todo in todos:
-        if todo.id == item_id:
-            return todo
-    return None
+def create(db: Session, todo_create: ToDoCreate) -> Todo:
+    new_todo = Todo(
+        **todo_create.model_dump()
+    )
+    db.add(new_todo)
+    db.commit()
+    return new_todo
 
-def find_by_name(name: str) -> List[Todo]:
-    return [todo for todo in todos if name in todo.name]
-
-def create(item_create: ToDoCreate) -> Todo:
-    new_id = max(todo.id for todo in todos) + 1
-    new_item = Todo(id=new_id, name=item_create.name, category=item_create.category, status=item_create.status, due_date=item_create.due_date)
-    todos.append(new_item)
-    return new_item
-
-def update(item_id: int, item_update: ToDoUpdate) -> Optional[Todo]:
-    todo = find_by_id(item_id)
-    if todo:
-        if item_update.name is not None:
-            todo.name = item_update.name
-        if item_update.category is not None:
-            todo.category = item_update.category
-        if item_update.status is not None:
-            todo.status = item_update.status
-        if item_update.due_date is not None:
-            todo.due_date = item_update.due_date
-        return todo
-    return None
-
-def delete(item_id: int) -> Optional[Todo]:
-    global todos
-    todo = find_by_id(item_id)
-    if todo:
-        todos = [i for i in todos if i.id != item_id]
-        return todo
-    return None
+def update(db: Session, id: int, todo_update: ToDoUpdate) -> Optional[Todo]:
+    todo = find_by_id(db, id)
+    if todo is None:
+        return None
+    
+    todo.name = (
+        todo.name 
+        if todo_update.name is None 
+        else todo_update.name
+    )
+    todo.category = (
+        todo.category 
+        if todo_update.category is None 
+        else todo_update.category
+    )
+    todo.status = (
+        todo.status 
+        if todo_update.status is None 
+        else todo_update.status
+    )
+    todo.due_date = (
+        todo.due_date 
+        if todo_update.due_date is None 
+        else todo_update.due_date
+    )
+    db.add(todo)
+    db.commit()
+    
+    return todo
+    
+    
+def delete(db:Session, id: int) -> Optional[Todo]:
+    todo = find_by_id(db, id)
+    if todo is None:
+        return None
+    db.delete(todo)
+    db.commit()
+    return todo
