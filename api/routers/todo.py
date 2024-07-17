4f4ce1ee7446e312ae5f3fd9c3184f6f4a42a031
mitsuki0215 <mitsuki0215>
2024-07-18 00:58:38 +0900

    todoの作成・取得・更新・削除・名前で検索機能のAPIを完成させた

--- api/routers/todo.py
+++ api/routers/todo.py
@@ -1,40 +1,49 @@
+from typing import Annotated
 from typing import List
-from fastapi import APIRouter, Path, Query, HTTPException
+from fastapi import APIRouter, Path, Query, HTTPException, Depends
+from sqlalchemy.orm import Session
 from starlette import status
 from cruds import todo as todo_cruds
 from schemas import ToDoCreate, ToDoUpdate, ToDoResponse
+from database import get_db
+
+
+DbDpendency = Annotated[Session, Depends(get_db)]
 
 router = APIRouter(prefix="/todos", tags=["Todos"])
 
 @router.get("", response_model=List[ToDoResponse], status_code=status.HTTP_200_OK)
-async def find_all():
-    return todo_cruds.find_all()
+async def find_all(db: DbDpendency):
+    return todo_cruds.find_all(db)
 
 @router.get("/{id}", response_model=ToDoResponse, status_code=status.HTTP_200_OK)
-async def find_by_id(id: int = Path(gt=0)):
-    found_todo = todo_cruds.find_by_id(id)
+async def find_by_id(db: DbDpendency, id: int = Path(gt=0)):
+    found_todo = todo_cruds.find_by_id(db, id)
     if not found_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return found_todo
 
-@router.get("/search", response_model=List[ToDoResponse], status_code=status.HTTP_200_OK)
-async def find_by_name(name: str = Query(min_length=2, max_length=20)):
-    return todo_cruds.find_by_name(name)
+@router.get("/", response_model=List[ToDoResponse], status_code=status.HTTP_200_OK)
+async def find_by_name(db: DbDpendency, name: str = Query(min_length=2, max_length=20)):
+    return todo_cruds.find_by_name(db, name)
 
 @router.post("", response_model=ToDoResponse, status_code=status.HTTP_201_CREATED)
-async def create(todo_create: ToDoCreate):
-    return todo_cruds.create(todo_create)
+async def create(db: DbDpendency, todo_create: ToDoCreate):
+    return todo_cruds.create(db, todo_create)
 
 @router.put("/{id}", response_model=ToDoResponse, status_code=status.HTTP_200_OK)
-async def update(todo_update: ToDoUpdate, id: int = Path(gt=0)):
-    updated_todo = todo_cruds.update(id, todo_update)
+async def update(db: DbDpendency, todo_update: ToDoUpdate, id: int = Path(gt=0)):
+    updated_todo = todo_cruds.update(db, id, todo_update)
     if not updated_todo:
-        raise HTTPException(status_code=404, detail="Item not updated")
+        raise HTTPException(
+            status_code=404, 
+            detail="Item not updated"
+        )
     return updated_todo
 
 @router.delete("/{id}", response_model=ToDoResponse, status_code=status.HTTP_200_OK)
-async def delete(id: int = Path(gt=0)):
-    deleted_todo = todo_cruds.delete(id)
+async def delete(db: DbDpendency, id: int = Path(gt=0)):
+    deleted_todo = todo_cruds.delete(db, id)
     if not deleted_todo:
         raise HTTPException(status_code=404, detail="Item not deleted")
     return deleted_todo
