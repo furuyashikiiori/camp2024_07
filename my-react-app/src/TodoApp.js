346178bcd9d011e41312409608f028f146a9c14f
mitsuki0215 <mitsuki0215>
2024-07-20 17:02:14 +0900

    React側から編集できるようにして、データベースにもそのデータが保存できるようになった

--- my-react-app/src/TodoApp.js
+++ my-react-app/src/TodoApp.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react';
-// import {} from 'react-router-dom';
 import axios from 'axios';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 import CategoryFilter from './components/CategoryFilter';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Calendar from './components/Calendar';
 import './App.css';
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [category, setCategory] = useState('All');
 
-  // ToDoのデータを取得
   useEffect(() => {
     axios.get('http://localhost:8000/todos')
       .then(response => {
@@ -24,7 +19,6 @@ const TodoApp = () => {
       });
   }, []);
 
-  // ToDoの追加
   const addTodo = (task) => {
     axios.post('http://localhost:8000/todos', {
       name: task.name,
@@ -40,7 +34,6 @@ const TodoApp = () => {
     });
   };
 
-  // ToDoの完了状態のトグル
   const toggleComplete = (id) => {
     const todoToToggle = todos.find(todo => todo.id === id);
     const updatedTask = {
@@ -57,7 +50,6 @@ const TodoApp = () => {
       });
   };
 
-  // ToDoの削除
   const deleteTodo = (id) => {
     axios.delete(`http://localhost:8000/todos/${id}`)
       .then(response => {
@@ -68,7 +60,6 @@ const TodoApp = () => {
       });
   };
 
-  // ToDoの編集
   const editTodo = (id, updatedTask) => {
     axios.put(`http://localhost:8000/todos/${id}`, updatedTask)
       .then(response => {
@@ -91,7 +82,6 @@ const TodoApp = () => {
           deleteTodo={deleteTodo}
           editTodo={editTodo}
          />
-          {/* <Route path="/calendar" element={<Calendar todos={todos} />} /> */}
       </div>
   );
 };
