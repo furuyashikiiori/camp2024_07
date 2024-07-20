b52c013dc12618b8fb3c6bb39d1160aba56cfc04
Nozomi Ishimaru <s2322096@stu.musashino-u.ac.jp>
2024-07-20 14:18:48 +0900

    gifの設定

--- my-react-app/src/components/GrowUP.js
+++ my-react-app/src/components/GrowUP.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import './GrowUP.css'; 
+import './GrowUP.css';
 
 const GrowUP = () => {
-  // Example gif URLs
-  const gifUrl = 'path_to_gif_image';
-  const gifType = 'type_of_gif'; // Example: "Egg", "Hatchling", "Adult"
-  const timeToGrow = '5 days'; // Example value, this should be dynamically calculated
-
   return (
     <div className="growup-container">
-      <h2>ユニコッちの成長</h2>
-      <div className="growup-status">
-        <div className="growup-gif">
-          {/* Insert gif image */}
-          {/* <img src={gifUrl} alt="ユニコッち" /> */}
-        </div>
-        <div className="growup-info">
-          <p><strong>名前:</strong> ユニコッち</p>
-          <p><strong>種類:</strong> {/* {gifType} */}</p>
-          <p><strong>次の成長まで:</strong> {/* {timeToGrow} */}</p>
-        </div>
+      <div className="gif-container">
+        {/* gif画像の表示 */}
+        <img src="./gif/egg.giff" alt="ユニコッち" />
       </div>
     </div>
   );
