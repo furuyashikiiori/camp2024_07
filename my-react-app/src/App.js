c00d72f43984959299c3cfbfe8ed473269e29a6e
s2322087 <s2322087@stu.musashino-u.ac.jp>
2024-07-21 03:17:21 +0900

    ログインできるように修正

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import TodoApp from "./TodoApp";
@@ -8,20 +7,25 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Calendar from "./components/Calendar";
 import GrowUP from "./components/GrowUP";
+import Login from "./components/Login"; // Loginコンポーネントをインポート
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const location = useLocation(); // useLocationフックを使用して現在のURLパスを取得
+  const isLoginPage = location.pathname === "/"; // 現在のページがログインページかどうかを判定
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8000/todos")
-      .then((response) => {
-        setTodos(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }, []);
+    if (!isLoginPage) { // ログインページでない場合にのみToDoのデータを取得
+      axios
+        .get("http://localhost:8000/todos")
+        .then((response) => {
+          setTodos(response.data);
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    }
+  }, [isLoginPage]);
 
   const addTodo = (task) => {
     setTodos([...todos, task]);
@@ -43,38 +47,46 @@ function App() {
     setTodos(todos.map((todo) => (todo.id === id ? updatedTask : todo)));
   };
 
-  // const taskCount = todos.filter(todo => todo.states === "タスク完了").length;
   const taskCount = todos.filter((todo) => todo.status === "タスク完了").length;
 
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      {!isLoginPage && (
         <header className="App-header">
           <Header />
         </header>
+      )}
 
-        {/* <Route>タグでパスに応じたコンポーネントを表示 */}
-        <Routes>
-          <Route
-            path="/todo"
-            element={
-              <TodoApp
-                todos={todos}
-                setTodos={setTodos}
-                addTodo={addTodo}
-                toggleComplete={toggleComplete}
-                deleteTodo={deleteTodo}
-                editTodo={editTodo}
-              />
-            }
-          />
-          <Route path="/calendar" element={<Calendar todos={todos} />} />
-          <Route path="/growup" element={<GrowUP taskCount={taskCount} />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/todo"
+          element={
+            <TodoApp
+              todos={todos}
+              setTodos={setTodos}
+              addTodo={addTodo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+            />
+          }
+        />
+        <Route path="/calendar" element={<Calendar todos={todos} />} />
+        <Route path="/growup" element={<GrowUP taskCount={taskCount} />} />
+      </Routes>
+
+      {!isLoginPage && <Footer />}
+    </div>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
